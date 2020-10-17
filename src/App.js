09cd9ee07b8b23b04f5547a5dd97d5badf453cc0
Deepak <17deepakrv5@gmail.com>
2020-10-17 17:44:59 +0530

    Added header portion with a dropdown menu containing the names of the countries fetched from an api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,48 @@
-import React from 'react';
+import { FormControl, MenuItem, Select } from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [countries, setCountries] = useState([]);
+   //https://disease.sh/v3/covid-19/countries
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+            const countries = data.map((country) => (
+              {
+                name: country.country, // this returns the name of the country
+                value: country.countryInfo.iso3, // this returns the 3 letter valuse of the country like IND, USA, etc... 
+              }
+            ));
+
+            setCountries(countries);
+
+        });
+    };
+    getCountriesData();
+  }, []);
+
   return (
     <div className="app">
-     <h1>Covid 19 tracker</h1>
+    <div className="app__header">
+    <h1>Covid 19 tracker</h1>
+      <FormControl className="app__dropdown">
+        <Select variant="outlined" value="abc">
+          {
+            countries.map(country => (<MenuItem value={country.value}>{country.name}</MenuItem>))
+          }
+          {/*<MenuItem value="worldwide">Example 1</MenuItem>
+          <MenuItem value="worldwide">Examinationsdjgni</MenuItem>
+          <MenuItem value="worldwide">India</MenuItem>
+          <MenuItem value="worldwide">America</MenuItem>*/}
+        </Select>
+      </FormControl>
+    </div>
+    
     </div>
   );
 }
